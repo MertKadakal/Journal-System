bd928b9f78ea11f735b136c4359e092760f17d9c
MertKadakal <mert.kadakal1629@gmail.com>
2024-08-03 01:28:52 +0300

    List komutu tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,61 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        String[] authors_file = FileInput.readFile("src\\IO_1\\author.txt", false, true);
+        //load the input files - create author objects - set the output path
+        String output = "src\\output.txt";
+        FileOutput.writeToFile(output, "", false, false);
+        String[] articles_txt = FileInput.readFile("src\\IO_1\\article1.txt", false, true);
+        String[] authors_file_txt = FileInput.readFile("src\\IO_1\\author.txt", false, true);
+        String[] commands_txt = FileInput.readFile("src\\IO_1\\command.txt", false, true);
+
         ArrayList<Author> authors_list = new ArrayList<>();
-        for (String line1 : authors_file) {
+        for (String line1 : authors_file_txt) {
             String[] line = line1.split(" ");
             authors_list.add(new Author(line[1], line[2], line[3], line[4], line[5]));
         }
 
         //add articles as a list to author objects
-        for (int i = 0; i < authors_file.length; i++) {
+        for (int i = 0; i < authors_file_txt.length; i++) {
             ArrayList<String> articles = new ArrayList<>();
-            if (!(authors_file[i].split(" ").length == 6)) {
+            if (!(authors_file_txt[i].split(" ").length == 6)) {
                 int j = 6;
-                while (!(j > authors_file[i].split(" ").length-1)) {
-                    articles.add(authors_file[i].split(" ")[j]);
+                while (!(j > authors_file_txt[i].split(" ").length-1)) {
+                    articles.add(authors_file_txt[i].split(" ")[j]);
                     j++;
                 }
             }
             authors_list.get(i).articles = articles;
         }
 
-        for (Author author : authors_list) {
-            System.out.println(author.articles);
+        //create a dictionary for articles
+        HashMap<String, String> articles_map = new HashMap<>();
+        for (String line : articles_txt) {
+            articles_map.put(line.split(" ")[1], line.split(" ")[2]);
+        }
+
+        //execute commands: list, completeAll, sortedAll, del sth
+        for (String command : commands_txt) {
+            System.out.println(command.split(" ")[0]);
+            switch (command.split(" ")[0]) {
+                case "list":
+                    FileOutput.writeToFile(output, "----------------------------------------------List---------------------------------------------", true, true);
+                    for (Author author : authors_list) {
+                        FileOutput.writeToFile(output, String.format("Author:%s  %s  %s %s  %s", author.id, author.name, author.university, author.department, author.email) , true, true);
+                        for (String article : author.articles) {
+                            FileOutput.writeToFile(output, String.format("+%s    %s", article, articles_map.get(article)) , true, true);
+                        }
+                        if (authors_list.indexOf(author) == authors_list.size()-1) {
+                            FileOutput.writeToFile(output, "" , true, false);
+                        }
+                        else {
+                            FileOutput.writeToFile(output, "" , true, true);
+                        }
+                    }
+                    FileOutput.writeToFile(output, "----------------------------------------------End---------------------------------------------" , true, true);
+                    break;
+            }
         }
     }
 }
